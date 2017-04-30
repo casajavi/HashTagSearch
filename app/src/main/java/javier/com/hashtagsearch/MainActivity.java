0b49c5f1cba003cf56ddfe8ae374e4309bdeb3ae
Javier Casaudoumecq <javier@casaudoumecq.me>
2017-04-30 13:26:10 -0400

    added fabric, created initial result view

--- app/src/main/java/javier/com/hashtagsearch/MainActivity.java
+++ app/src/main/java/javier/com/hashtagsearch/MainActivity.java
@@ -1,24 +1,26 @@
 package javier.com.hashtagsearch;
 
-import android.app.SearchManager;
-import android.content.Context;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
-import android.support.design.widget.CoordinatorLayout;
-import android.support.design.widget.Snackbar;
-import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.SearchView;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
-import android.widget.TextView;
 import android.widget.Toast;
+import com.twitter.sdk.android.Twitter;
+import com.twitter.sdk.android.core.TwitterAuthConfig;
+
+import butterknife.ButterKnife;
+import io.fabric.sdk.android.Fabric;
+import javier.com.hashtagsearch.fragments.SearchResultsFragment;
 
 public class MainActivity extends AppCompatActivity {
 
-    private TextView mTextMessage;
+    // Note: Your consumer key and secret should be obfuscated in your source code before shipping.
+    private static final String TWITTER_KEY = "pp2tpNNPnWwh3Rzn5vyl8kIYG";
+    private static final String TWITTER_SECRET = "4Bdc1O7lggk10SYtP5B2YxTQOVjo4WOHOK85UV6G7WNRBQGuLF";
+
 
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
             = new BottomNavigationView.OnNavigationItemSelectedListener() {
@@ -27,10 +29,10 @@ public class MainActivity extends AppCompatActivity {
         public boolean onNavigationItemSelected(@NonNull MenuItem item) {
             switch (item.getItemId()) {
                 case R.id.navigation_search:
-                    mTextMessage.setText(R.string.title_search);
+                    // Show Current Search
                     return true;
                 case R.id.navigation_history:
-                    mTextMessage.setText(R.string.title_history);
+                    // Show Search History
                     return true;
             }
             return false;
@@ -42,10 +44,13 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        TwitterAuthConfig authConfig = new TwitterAuthConfig(TWITTER_KEY, TWITTER_SECRET);
+        Fabric.with(this, new Twitter(authConfig));
         setContentView(R.layout.activity_main);
 
-        mTextMessage = (TextView) findViewById(R.id.message);
-        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
+        getSupportFragmentManager().beginTransaction().add(R.id.content, new SearchResultsFragment(),SearchResultsFragment.class.getSimpleName()).commit();
+
+        BottomNavigationView navigation = ButterKnife.findById(this, R.id.navigation);
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
     }
 
